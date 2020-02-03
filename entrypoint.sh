3c762b6c6ad90c14ae95d9345ac2443f807e96ee
Wizos <wizos@users.noreply.github.com>
2020-02-03 16:09:34 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,4 @@
-cd /gost
-wget -O gost.gz  http://github.com/ginuerzh/gost/releases/download/v${VER}/gost-linux-amd64-${VER}.gz
+wget -O gost.gz http://github.com/ginuerzh/gost/releases/download/v${VER}/gost-linux-amd64-${VER}.gz
 gzip -d gost.gz
 mv gost-linux-amd64-${VER} gost
 chmod +x gost

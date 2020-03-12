f8ad7da86a231e6cdd53ba644d9d55c462ad8571
Wizos <wizos@users.noreply.github.com>
2020-03-12 20:36:57 +0800

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 wget -O gost.gz http://github.com/ginuerzh/gost/releases/download/v${VER}/gost-linux-amd64-${VER}.gz
 gzip -d gost.gz
-mv gost-linux-amd64-${VER} gost
+mv gost-linux-amd64 gost
 chmod +x gost
-./gost -L=${MODE}://${METHOD}:${PASSWORD}@:${PORT}
+./gost -L=tls://:${TLS_PORT}/:$PORT -L=ss+mws://$METHOD:$PASSWORD@:$PORT
